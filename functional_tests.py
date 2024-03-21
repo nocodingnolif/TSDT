5eb838c7f6fa9f872d1794e7759b073a05191d2e
yolo <3111611519@qq.com>
2024-03-21 16:07:32 +0800

    message 2:the first functional test

--- functional_tests.py
+++ functional_tests.py
@@ -1,8 +1,13 @@
 from selenium import webdriver
-from selenium.webdriver.firefox.options import Options
-
-options = Options()
-options.binary_location = r'C:\Program Files\Mozilla Firefox\firefox.exe'
-driver = webdriver.Firefox(options=options, executable_path="E:/xuexi/firefox/geckodriver.exe")
-driver.get("https://localhost：8000")
-assert 'Django' in browser.page_source
+import unittest
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        def tearDown(self):
+            self.browser.quit()
+            def test_can_start_a_list_and_retrieve_it_later(self):
+                self.browser.get("https://localhost：8000")
+                self.assertIn('To-Do',self.browser.title),"browser title was:" + self.browser.title
+                self.fail('Finish the test!')
+                if __name__== '__main__':
+                    unittest.main()
