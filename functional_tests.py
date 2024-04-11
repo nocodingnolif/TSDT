e64ef93244641e7d1890f56568a64fc83e181da4
yolo <3111611519@qq.com>
2024-04-11 15:29:34 +0800

    message 8 :Before refactoring the functional_tests

--- functional_tests.py
+++ functional_tests.py
@@ -3,29 +3,33 @@ from selenium.webdriver.common.keys import Keys
 import time
 import unittest
 from selenium.webdriver.common.by import By
+
 class NewVisitorTest(unittest.TestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
+
     def tearDown(self):
         self.browser.quit()
+
     def test_can_start_a_list_and_retrieve_it_later(self):
-        self.browser.get("https://localhost：8000")
-        self.assertIn('To-Do',self.browser.title)
-        header_text = self.browser.find_element(By.TAG_NAME,'h1').text
-        self.assertIn('To-Do',header_text)
-        inputbox = self.browser.find_element(By.ID,'id_new_item')
-        self.assertEqual(
-                inputbox.get_attribute('placeholder'),
-                'Enter a to-do item'
-        )
-        inputbox.send_keys('Buy flowers')
+        self.browser.get("http://localhost:8000")
+        self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element(By.TAG_NAME, 'h1').text
+        self.assertIn('To-Do', header_text)
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        inputbox.send_keys('Give a gift to Lisi')
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
-        table = self.browser.find_element(By.ID,'id_list_table')
-        rows = table.find_element(By.TAG_NAME,'tr')
+
+
+
+        table = self.browser.find_element(By.ID, 'id_list_table')
+        rows = table.find_elements(By.TAG_NAME, 'tr')  # Corrected this line
         self.assertIn('1: Buy flowers', [row.text for row in rows])
+        self.assertIn('2: Give a gift to Lisi', [row.text for row in rows])
         self.fail('Finish the test!')
 
-if __name__== '__main__':
+
+if __name__ == '__main__':
     unittest.main()
