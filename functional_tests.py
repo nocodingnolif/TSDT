2953cb9452b87dc3bfb6c6037fac33d03d7d9313
yolo <3111611519@qq.com>
2024-04-18 16:14:48 +0800

    message 11: Show all items in template

--- functional_tests.py
+++ functional_tests.py
@@ -7,35 +7,34 @@ from selenium.webdriver.common.by import By
 class NewVisitorTest(unittest.TestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
-
     def tearDown(self):
         self.browser.quit()
-
     def check_for_row_in_list_table(self, row_text):
         table = self.browser.find_element(By.ID,'id_list_table')
         rows = table.find_elements(By.TAG_NAME,'tr')
         self.assertIn(row_text, [row.text for row in rows])
-    
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get("http://localhost:8000")
-        self.assertIn('To-Do', self.browser.title)
-        header_text = self.browser.find_element(By.TAG_NAME, 'h1').text
-        self.assertIn('To-Do', header_text)
-        inputbox = self.browser.find_element(By.ID, 'id_new_item')
-        inputbox.send_keys('Give a gift to Lisi')
+        self.assertIn('To-Do', self.browser.title), 
+        header_text=self.browser.find_element(By.TAG_NAME,'h1').text
+        self.assertIn('To-Do',header_text)
+        inputbox=self.browser.find_element(By.ID,'id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'),'Enter a to-do item')
+        inputbox.send_keys('Buy flowers')
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
         self.check_for_row_in_list_table('1: Buy flowers')
-
-        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        inputbox=self.browser.find_element(By.ID,'id_new_item')
         inputbox.send_keys('Give a gift to Lisi')
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
-        self.check_for_row_in_list_table('1:Buy flowers')
+
+        self.check_for_row_in_list_table('1: Buy flowers')
         self.check_for_row_in_list_table('2: Give a gift to Lisi')
-       
+
         self.fail('Finish the test!')
+
 
 
 if __name__ == '__main__':
