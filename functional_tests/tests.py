4094ed05195b52c7a8faa446f1542c712a10672f
yolo <3111611519@qq.com>
2024-05-23 15:19:39 +0800

    message 23: FT can test real_server

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -5,12 +5,18 @@ from selenium.webdriver.common.by import By
 #from django.test import LiveServerTestCase
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium.common.exceptions import WebDriverException
+import os
 
 MAX_WAIT = 10
 
 class NewVisitorTest(StaticLiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
+        real_server = '172.25.173.61'
+        if real_server:
+            self.live_server_url = 'http://' + real_server
+
+
 
     def tearDown(self):
         self.browser.quit()
